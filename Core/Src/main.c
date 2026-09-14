493391ded0530e33c85c98b14db71cf6a7ddf90c
Bob <Bob@Vitalik>
2026-09-14 22:11:21 +0300

    display

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,6 +22,9 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include<stdio.h>
+#include "ssd1306.h"
+#include "ssd1306_tests.h"
+#include "ssd1306_fonts.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -40,6 +43,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
 
 /* USER CODE BEGIN PV */
 
@@ -48,21 +52,19 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
 /* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
   * @retval int
   */
-
-
 int main(void)
 {
 
@@ -81,7 +83,7 @@ int main(void)
 	    rows[3] = HAL_GPIO_ReadPin(KEY_ROW_4_GPIO_Port, KEY_ROW_4_Pin);
 	}
 
-	char readKeypad(void) {
+	void readKeypad(void) {
 	    char keypadMap[4][4] = {
 	        {'1', '2', '3', 'A'},
 	        {'4', '5', '6', 'B'},
@@ -107,15 +109,19 @@ int main(void)
 	            if (HAL_GPIO_ReadPin(rowPorts[r], rowPins[r]) == GPIO_PIN_RESET) {
 	                //col back to 1
 	                HAL_GPIO_WritePin(colPorts[c], colPins[c], GPIO_PIN_SET);
-	                return keypadMap[r][c];
+					ssd1306_Fill(Black);
+
+					ssd1306_SetCursor(0, 0);
+
+					ssd1306_WriteChar(keypadMap[r][c], Font_16x24, White);
+
+					ssd1306_UpdateScreen();
 	            }
 	        }
 
 	        //back to 1
 	        HAL_GPIO_WritePin(colPorts[c], colPins[c], GPIO_PIN_SET);
 	    }
-
-	    return '\0';
 	}
 
   /* USER CODE END 1 */
@@ -138,16 +144,17 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
-
+	ssd1306_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 	while (1) {
     /* USER CODE END WHILE */
-		printf("KEY: %c\n", readKeypad());
-		HAL_Delay(1000);
+		readKeypad();
+ 		HAL_Delay(100);
     /* USER CODE BEGIN 3 */
 	}
   /* USER CODE END 3 */
@@ -197,6 +204,40 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 40000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
